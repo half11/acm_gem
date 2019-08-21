6c91f800754555e3ed9fc9ee10d56b56c1fb74f6
Fred CM <fredoacmk@gmail.com>
2019-08-20 22:57:21 -0500

    initial commit

---
+++ lib/view_tool_acm.rb
@@ -0,0 +1,7 @@
+require "view_tool_acm/version"
+require "view_tool_acm/renderer"
+
+module ViewToolAcm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/view_tool_acm/version.rb
@@ -0,0 +1,3 @@
+module ViewToolAcm
+  VERSION = "0.1.0"
+end
